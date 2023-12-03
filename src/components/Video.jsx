6f1e8bd991ce12563104b53091016f1828bacb26
IsaacManzo <isaacemanzo51@gmail.com>
2023-12-03 13:46:33 -0300

    videofixed

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
     <div className="flex justify-center pt-10 pb-10">
       <div className="max-w-screen-sm lg:max-w-screen-lg w-full overflow-hidden shadow-lg relative ">
         <ReactPlayer
-          url="https://www.youtube.com/watch?v=DXrofRpkieQ&t=17s&ab_channel=IsaacManzo"
+          url="https://www.youtube.com/watch?v=DXrofRpkieQ&t=0s&ab_channel=IsaacManzo"
           controls
           width="100%"
           height="433px"
